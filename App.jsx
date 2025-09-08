1efc05fe55ee9e51a2a9f0050581114722dadd19
Abhishek Malkhedkar <abhishekmalkhedkar3@gmail.com>
2025-09-08 15:36:43 +0530

    Flex Intro

--- App.jsx
+++ App.jsx
@@ -6,40 +6,57 @@
  */
 
 import React from 'react';
-import { Button, Text, View, TextInput} from 'react-native';
-import Student from './components/Student'
+import {  View, StyleSheet  } from 'react-native';
 
-class App extends React.Component {
-
-  constructor(){
-    super();
-    this.state = {
-      name : "Abhi",
-    }
-  }
-
-  updateName(val){
-    this.setState({name : val})
-  }
-
-  fruit = () => {
-    console.warn("Apple");
-  }
-
-  render(){
-    return (
-      <View>
-        <Text style={{fontSize : 30, color : 'red'}}>Class Component</Text>
-        <Text style={{fontSize : 30, color : 'red'}}>{this.state.name}</Text>
-        <TextInput placeholder='Enter Here' onChangeText={(text) => this.updateName(text)} />
-        <Button title='Press here' onPress={this.fruit} />
-        <Student name={this.state.name} />
+const App = () => {
+  return (
+    <View style={styles.main}>
+      <View style={styles.box1}>
+        <View style={styles.InnerBox1}></View>
+        <View style={styles.InnerBox2}></View>
+        <View style={styles.InnerBox3}></View>
       </View>
-    )
-  }  
-
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+    </View>
+  )
 }
-  
+
+const styles = StyleSheet.create({
+  main : {
+    flex : 1,
+  },
+  box1 : {
+    flex : 2,
+    backgroundColor : 'red',
+    flexDirection : 'row'
+  },
+  box2 : {
+    flex : 1,
+    backgroundColor : 'green'
+  },
+  box3 : {
+    flex : 1,
+    backgroundColor : 'blue'
+  },
+  InnerBox1 : {
+    flex : 1,
+    backgroundColor : 'skyblue',
+    margin : 10
+  },
+  InnerBox2 : {
+    flex : 1,
+    backgroundColor : 'grey',
+    margin : 10
+  },
+  InnerBox3 : {
+    flex : 1,
+    backgroundColor : 'orange',
+    margin : 10
+  }
+})
+
+
 
 
 export default App;
