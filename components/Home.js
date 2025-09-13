726d1dc88fec20e013f66107c665bee4c2610cdf
Abhishek Malkhedkar <abhishekmalkhedkar3@gmail.com>
2025-09-13 17:41:11 +0530

    Passing Data bw Screens

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native";
 
-const Home = () => {
+const Home = (prop) => {
+
+    const {name , age} = prop.route.params;
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 30 }}>Home Screen</Text>
+      <Text style={{ fontSize: 30 }}>Name :  {name}</Text>
+      <Text style={{ fontSize: 30 }}>Age : {age}</Text>
     </View>
   );
 };
--- components/Login.js
+++ components/Login.js
@@ -1,12 +1,19 @@
-import { Button, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 const Login = props => {
+
+    const age = 29;
+
+    const [name, setName] = useState('');
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 30 }}>Login Screen</Text>
+      <TextInput onChangeText={(text) => setName(text) } placeholder="Enter Name" style={{fontSize : 20, borderColor : '#000', borderWidth : 2}}  />
       <Button
         title="Go to Home Page"
-        onPress={() => props.navigation.navigate('Home')}
+        onPress={() => props.navigation.navigate('Home', {name  , age })}
       />
     </View>
   );
