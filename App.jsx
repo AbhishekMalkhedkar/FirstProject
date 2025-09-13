bdc4799f84cb149056c75aa740c7205b978ed5e8
Abhishek Malkhedkar <abhishekmalkhedkar3@gmail.com>
2025-09-13 16:40:44 +0530

    Stack Navigation style

--- App.jsx
+++ App.jsx
@@ -13,32 +13,57 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Home' component={Home} />
+      <Stack.Navigator
+      screenOptions={{
+             headerStyle : {
+              backgroundColor : 'grey',
+             },
+             headerTitleStyle : {
+              fontSize : 25
+             },
+             headerTintColor : 'skyblue'
+
+             }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          
+        />
+        <Stack.Screen name="Home" component={Home}
+        options={{
+             title: 'User Home',
+             headerStyle : {
+              backgroundColor : 'lightblue',
+             },
+             headerTitleStyle : {
+              fontSize : 26
+             },
+             headerTintColor : 'grey'
+
+             }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-
 const Home = () => {
-  return(
-    <View style={{flex : 1, justifyContent : 'center', alignItems : 'center'}}>
-      <Text style={{fontSize : 30}}>Home Screen</Text>
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30 }}>Home Screen</Text>
     </View>
-  )
-}
-const Login = (props) => {
-  return(
-    <View style={{flex : 1, justifyContent : 'center', alignItems : 'center'}}>
-      <Text style={{fontSize : 30}}>Login Screen</Text> 
-      <Button title='Go to Home Page' onPress={() => props.navigation.navigate("Home")} />
+  );
+};
+const Login = props => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30 }}>Login Screen</Text>
+      <Button
+        title="Go to Home Page"
+        onPress={() => props.navigation.navigate('Home')}
+      />
     </View>
-  )
-}
-
-
-
+  );
+};
 
 export default App;
