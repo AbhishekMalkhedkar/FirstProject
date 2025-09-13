77887682830b6a867c22018fde4a79a5ff720012
Abhishek Malkhedkar <abhishekmalkhedkar3@gmail.com>
2025-09-13 15:04:13 +0530

    Custom Modal

--- App.jsx
+++ App.jsx
@@ -5,17 +5,47 @@
  * @format
  */
 
-import { View} from 'react-native';
-import {WebView } from 'react-native-webview'
+import { useState } from 'react';
+import { View, StyleSheet, Button, Text } from 'react-native';
 
 const App = () => {
-
+  const [show, setShow] = useState(false);
 
   return (
-    <WebView source={{uri : "https://www.google.com/"}} />
+    <View style={styles.container}>
+      {show ? (
+        <View style={styles.modal}>
+          <View style={styles.body}>
+            <Text>Custom Modal</Text>
+            <Button title="Close" onPress={() => setShow(false)} />
+          </View>
+        </View>
+      ) : null}
+
+      <Button title="Open Dialog" onPress={() => setShow(true)} />
+    </View>
   );
 };
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: 'rgba(50,50,50,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  body: {
+    backgroundColor: '#fff',
+    height: 300,
+    width: 300,
+    padding: 20,
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+});
 
 export default App;
